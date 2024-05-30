9ec21f2d05ac5d0069ef612eb536f1a53883d004
Kushal Amara Chandrashekar <95076451+kushalac@users.noreply.github.com>
2024-05-31 02:55:50 +0530

    Update online_book_store_db.sql

--- online_book_store_db.sql
+++ online_book_store_db.sql
@@ -73,18 +73,6 @@ CREATE TABLE `authors` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `authors`
---
-
-INSERT INTO `authors` (`id`, `name`) VALUES
-(3, 'Navathe'),
-(4, 'Ramakrishnan'),
-(5, 'Sadhguru'),
-(6, 'Carl Sagan'),
-(7, 'AL Sweigrat'),
-(8, 'Randal K Michael'),
-(10, '5th sem');
 
 --
 -- Triggers `authors`
@@ -107,18 +95,7 @@ CREATE TABLE `authors_backup` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `authors_backup`
---
 
-INSERT INTO `authors_backup` (`id`, `name`) VALUES
-(3, 'Navathe'),
-(4, 'Ramakrishnan'),
-(5, 'Sadhguru'),
-(6, 'Carl Sagan'),
-(7, 'AL Sweigrat'),
-(8, 'Randal K Michael'),
-(10, '5th sem'),
 (12, 'Robert');
 
 -- --------------------------------------------------------
@@ -137,19 +114,6 @@ CREATE TABLE `books` (
   `file` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`id`, `title`, `author_id`, `description`, `category_id`, `cover`, `file`) VALUES
-(3, 'Fundamentals of DataBase', 3, 'This book introduces the fundamental concepts  necessary for designing, using, and implementing  database systems and database applications.', 3, '61cd6223e59099.75326708.jpeg', '61cd6223e5b380.97936895.pdf'),
-(4, 'Fundamentals of DataBase', 4, 'principles of database systems and emphasizes how they are used in developing data intensive applications', 3, '61cd63262be128.55348934.jpeg', '61cd63262bfee9.37753100.pdf'),
-(5, 'SHIVA', 5, 'ultimate outlaw', 4, '61cd63f9625281.09281952.jpeg', '61cd63f962c360.17568486.pdf'),
-(6, 'Kozmos', 6, 'Astronomy and Astrophysics', 5, '61cd68b1a1f9f6.25828165.jpeg', '61cd68b1a21889.66774974.pdf'),
-(7, 'Automate the boring stuff with python', 7, 'Practical programming for total beginners in python', 6, '61cd66688bbd88.91013643.jpeg', '61cd66688bd871.27484858.pdf'),
-(8, 'New Millennium', 10, 'Astronomy and Astrophysics', 9, '61cd68e11a6964.65708461.jpeg', '61cd68e11a8673.53081321.pdf'),
-(9, 'Unix Shell Scripting', 8, 'Unix programming basics', 8, '61cd69937e9fb2.55013660.jpeg', '61cd69937ebe70.98979426.pdf'),
-(10, 'BookDrive', 10, 'Bookdrive is an online bookstore which allows users to access a lot of books at ease', 9, '61cd6cc7ca1097.10965019.jpeg', '61cd6cc7ca3279.75726780.pdf');
 
 --
 -- Triggers `books`
@@ -176,22 +140,6 @@ CREATE TABLE `books_backup` (
   `file` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `books_backup`
---
-
-INSERT INTO `books_backup` (`id`, `title`, `author_id`, `description`, `category_id`, `cover`, `file`) VALUES
-(3, 'Fundamentals of DataBase', 3, 'This book introduces the fundamental concepts  necessary for designing, using, and implementing  database systems and database applications.', 3, '61cd6223e59099.75326708.jpeg', '61cd6223e5b380.97936895.pdf'),
-(4, 'Fundamentals of DataBase', 4, 'principles of database systems and emphasizes how they are used in developing data intensive applications', 3, '61cd63262be128.55348934.jpeg', '61cd63262bfee9.37753100.pdf'),
-(5, 'SHIVA', 5, 'ultimate outlaw', 4, '61cd63f9625281.09281952.jpeg', '61cd63f962c360.17568486.pdf'),
-(6, 'Kozmos', 6, 'Astronomy and Astrophysics', 5, '61cd68b1a1f9f6.25828165.jpeg', '61cd68b1a21889.66774974.pdf'),
-(7, 'Automate the boring stuff with python', 7, 'Practical programming for total beginners in python', 6, '61cd66688bbd88.91013643.jpeg', '61cd66688bd871.27484858.pdf'),
-(8, 'New Millennium', 6, 'Astronomy and Astrophysics', 5, '61cd68e11a6964.65708461.jpeg', '61cd68e11a8673.53081321.pdf'),
-(9, 'Unix Shell Scripting', 8, 'Unix programming basics', 8, '61cd69937e9fb2.55013660.jpeg', '61cd69937ebe70.98979426.pdf'),
-(10, 'BookDrive', 10, 'Bookdrive is an online bookstore which allows users to access a lot of books at ease', 9, '61cd6cc7ca1097.10965019.jpeg', '61cd6cc7ca3279.75726780.pdf'),
-(12, 'E-Commerce', 10, 'Furniture Store', 9, '61cd930022c167.79640583.jpeg', '61cd9300234902.56970183.pdf'),
-(14, 'Book Drive Report', 10, 'This gives the idea on the project Book Drive', 9, '6241e9208d1793.39791745.jpeg', '6241e9208da5a6.73929834.pdf');
-
 -- --------------------------------------------------------
 
 --
@@ -202,19 +150,6 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `name`) VALUES
-(3, 'DBMS'),
-(4, 'Motivation'),
-(5, 'Astronomy'),
-(6, 'Python'),
-(8, 'Unix Programming'),
-(9, 'Research'),
-(10, 'stories');
 
 --
 -- Triggers `categories`
@@ -237,19 +172,6 @@ CREATE TABLE `categories_backup` (
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `categories_backup`
---
-
-INSERT INTO `categories_backup` (`id`, `name`) VALUES
-(3, 'DBMS'),
-(4, 'Motivation'),
-(5, 'Astronomy'),
-(6, 'Python'),
-(8, 'Unix Programming'),
-(9, 'Research'),
-(10, 'stories'),
-(14, 'Commerce');
 
 --
 -- Indexes for dumped tables
